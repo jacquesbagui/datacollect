ec53c1ec4ec7be9dae5c2a91eac8fbd618f870b5
Jean Jacques BAGUI <jacques.bagui@gmail.com>
2025-10-19 15:18:51 +0200

    update app

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -9,8 +9,8 @@ import { ELECTION_CONFIG } from '@/utils/constants';
       <div class="flex items-center justify-between">
         <!-- Logo et titre -->
         <div class="flex items-center space-x-3">
-          <div class="bg-orange-600 p-2 rounded-lg">
-            <Vote class="h-8 w-8 text-white" />
+          <div class="bg-white-600 p-2 rounded-lg">
+            <img src="/logo-civ.png" alt="Logo" class="w-16" />
           </div>
           <div>
             <h1 class="text-2xl font-bold text-gray-900">{{ ELECTION_CONFIG.TITLE }}</h1>
@@ -23,7 +23,7 @@ import { ELECTION_CONFIG } from '@/utils/constants';
         <!-- Badge élection -->
         <div class="flex items-center space-x-2">
           <span class="px-3 py-1 bg-green-100 text-green-800 rounded-full text-sm font-medium">
-            {{ ELECTION_CONFIG.TITLE }}
+            {{ ELECTION_CONFIG.YEAR }}
           </span>
         </div>
       </div>
